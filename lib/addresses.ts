5076ea69bd687286a9164447ede8f5721e992a5f
sorxcode <sorxcode@gmail.com>
2022-04-06 06:54:54 +0100

    completed workshop

---
+++ lib/addresses.ts
@@ -0,0 +1,6 @@
+import { PublicKey } from "@solana/web3.js"
+
+// Your shop wallet address
+export const shopAddress = new PublicKey('55UVStcbBfwVnpn1v9ssMpkX6rSte7UPZRYn528UTWj')
+export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+export const couponAddress = new PublicKey('eDQtfQnFwpD5Hjz1gqFsv6mqfW9zCeXR6mtXEKFbUTC')
